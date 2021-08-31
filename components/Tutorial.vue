b73ed897ed9114b1b5eea739028100383f00fee1
Zakir Hossen <web.zakirbd@gmail.com>
2021-08-31 13:37:38 +0600

    file init

---
+++ components/Tutorial.vue
@@ -0,0 +1,6 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta harum corrupti ex vero fugit perferendis iste, deleniti quae, corporis delectus, ipsa architecto tenetur quas officia quisquam unde. A, perferendis quasi.
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Tutorial/>
+    <nuxt-content :document="page" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content,  error }) {
+    const page = await $content('hello')
+      .fetch()
+      .catch(err => {
+        error({ statusCode: 404, message: "Page not found" });
+      });
+
+    return { page };
+  }
+};
+
+</script>
